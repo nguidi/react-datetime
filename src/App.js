4b33308563eb149d2f6fccab801ecf04baf22828
nguidi <neri.guidi@gmail.com>
2022-07-19 09:22:58 -0300

    arqex/react-datetime re-bumped

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// This file is the playground used for development purposes (npm run playground)
+// not part of the library
+import React from 'react';
+import Datetime from './DateTime/DateTime.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './DateTime.scss'
+
+import moment from 'moment';
+import 'moment/locale/es';
+moment.locale('es');
+
+class App extends React.Component { 
+	state = {
+		date: new Date()
+	}
+
+	render() {
+		return (
+			<div>
+				<Datetime locale="es" />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/lib.package.js
@@ -0,0 +1,2 @@
+import DateTime from './DateTime/DateTime.js';
+export default DateTime;
